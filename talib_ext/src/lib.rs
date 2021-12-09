fcc0ce3cb1475d8f12be7f2421f19b2e4e89384b
Sacha Arbonel <sacha.arbonel@hotmail.fr>
2021-12-08 23:56:57 -0400

    looks like it compiles?

--- talib_ext/src/lib.rs
+++ talib_ext/src/lib.rs
@@ -1,10 +1,25 @@
 use pgx::*;
+use talib::Macd;
 
 pg_module_magic!();
 
 #[pg_extern]
 fn hello_talib_ext() -> &'static str {
     "Hello, talib_ext"
+}
+
+#[pg_extern]
+fn macd(
+    inReal: Vec<f64>,
+    inFastPeriod: i64,
+    inSlowPeriod: i64,
+    inSignalPeriod: i64,
+) -> (
+    name!(macd, Vec<f64>),
+    name!(macdsignal, Vec<f64>),
+    name!(macdhist, Vec<f64>),
+) {
+   Macd(&inReal, inFastPeriod, inSlowPeriod, inSignalPeriod)
 }
 
 #[cfg(any(test, feature = "pg_test"))]
@@ -16,7 +31,6 @@ mod tests {
     fn test_hello_talib_ext() {
         assert_eq!("Hello, talib_ext", crate::hello_talib_ext());
     }
-
 }
 
 #[cfg(test)]
